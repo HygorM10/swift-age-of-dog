febc4b96440d9575a38e182890f66164b1ca14cc
Hygor Martins <hygor.martins74@gmail.com>
2022-03-04 20:48:09 -0300

    first commit

--- Idade Dogs/ViewController.swift
+++ Idade Dogs/ViewController.swift
@@ -8,6 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textIdade: UITextField!
+    
+    @IBOutlet weak var labelResultadoIdade: UILabel!
+    
+    @IBAction func descobrirIdade(_ sender: Any) {
+        
+        guard let idade = textIdade.text else {
+            labelResultadoIdade.text = "Oppsss, digite uma idade valida!"
+            return
+        }
+        
+        if idade == "" {
+            labelResultadoIdade.text = "Oppsss, digite uma idade valida!"
+            return
+        }
+        
+        let resultado = Int(idade)! * 7
+        
+        labelResultadoIdade.text = "A idade do seu doguinho é: " + String(resultado)
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
